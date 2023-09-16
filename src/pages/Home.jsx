f601bf6f0633a642a51eb409e8013b7f02786966
Vibhour Sharma <vibhour121@gmail.com>
2023-09-16 20:33:07 +0530

    passesd values to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,21 @@ import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [bodyPart, setbodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setexercises={setExercises}
+        bodyPart={bodyPart}
+        setbodyPart={setbodyPart}
+      />
+      <Exercises
+        setexercises={setExercises}
+        bodyPart={bodyPart}
+        setbodyPart={setbodyPart}
+      />
     </Box>
   );
 };
