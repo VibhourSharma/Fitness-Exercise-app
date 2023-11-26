042ca2cfe98d52bfb441869d3c4962a0c100369f
Vibhour Sharma <vibhour121@gmail.com>
2023-11-26 23:55:38 +0530

    added api key in different env file✅

--- src/utils/FetchData.jsx
+++ src/utils/FetchData.jsx
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
